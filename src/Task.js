3bac51d6293036982b24a300a174f602a3587e82
Muhammad Husein <2010512130@mahasiswa.upnvj.ac.id>
2023-06-19 16:33:43 +0700

    add url in task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://monumental-cassata-216b24.netlify.app/"; // TODO: replace this
 const studentName = "Muhammad Husein"; // TODO: replace this
 const studentId = "FE5193596"; // TODO: replace this
 
