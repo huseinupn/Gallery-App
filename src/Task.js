7745ffa75d50d5ba875c6f6baf786765af596111
Muhammad Husein <2010512130@mahasiswa.upnvj.ac.id>
2023-06-19 16:12:44 +0700

    initial commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Muhammad Husein"; // TODO: replace this
+const studentId = "FE5193596"; // TODO: replace this
+
+export { url, studentName, studentId };
